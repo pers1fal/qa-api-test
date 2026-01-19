fd1984717fc0423326f311f41bc523ddafdc91c5
Nazar Yaremchuk <nazzik173@gmail.com>
2026-01-19 21:16:02 +0200

    Add parametrized test for get user by id

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,16 +1,19 @@
-def test_get_user_by_id(api_client):
-    response = api_client.get("/users/1")
+import pytest
+
+@pytest.mark.parametrize("user_id", [1, 2, 3])
+def test_get_user_by_id(api_client, user_id):
+    response = api_client.get(f"/users/{user_id}")
 
     assert response.status_code == 200
 
     data = response.json()
+    
     assert isinstance(data, dict)
 
-    assert data["id"] == 1
+    assert data["id"] == user_id
     assert "name" in data
     assert "username" in data
     assert "email" in data
-
     assert "address" in data
     assert "company" in data
 
