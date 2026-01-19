3eb9bd1bba344414f388cdf0478f61cffdaeadf4
Nazar Yaremchuk <nazzik173@gmail.com>
2026-01-19 21:07:55 +0200

    Add negative test for get user not found

---
+++ tests/test_user_not_found.py
@@ -0,0 +1,8 @@
+def test_get_user_not_found(api_client):
+    response = api_client.get("/users/9999")
+
+    assert response.status_code == 404
+
+    data = response.json()
+
+    assert data == {}
